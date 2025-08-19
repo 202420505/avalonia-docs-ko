34b9d205d00d9352b6168f5734017ad3ce4f5c5e
202420505 <170919602+202420505@users.noreply.github.com>
2025-08-19 16:00:20 +0900

    first commit

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Highlight from '@site/src/components/Highlight';
+import { CardSection, Card } from '../components/CardComponents'; 
+import HelpNeeded from '../components/HelpNeeded'; 
+import GitHubSampleLink from '../components/GitHubSampleLink';
+import MinVersion from '../components/MinVersion';
+import XpfAd from '../components/XpfAdvert';
+import Button from '../components/Button';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
+import ThemedImage from '@theme/ThemedImage';
+import Shortcut from '../components/Shortcut';
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  // Map the "<Highlight>" tag to our Highlight component
+  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
+  Highlight,
+  CardSection,
+  GitHubSampleLink,
+  Card,
+  HelpNeeded,
+  MinVersion,
+  XpfAd,
+  Button,
+  Tabs,
+  TabItem,
+  ThemedImage,
+  Shortcut
+};
